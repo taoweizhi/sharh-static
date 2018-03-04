fd886191fda3df302214c8507740c2efed9cf3b4
Christophe1997 <sdl_1997@163.com>
2018-03-04 15:55:28 +0800

    change structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 const gutil = require('gulp-util');
 
-gulp.task('Compress', () => {
-  gulp.src('dist/src/*.js')
+gulp.task('CompressJS', () => {
+  gulp.src('dist/src/js/*.js')
     .pipe(uglify())
     .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .pipe(gulp.dest('static/'))
+    .pipe(rename( path => {
+      if (!path.basename.endsWith('.min'))
+        path.basename += '.min'
+    }))
+    .pipe(gulp.dest('static/js/'))
+});
+
+gulp.task('CompressCSS', () => {
+  gulp.src('src/css/*.css')
+    .pipe(cleanCSS())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(rename( path => {
+      if (!path.basename.endsWith('.min'))
+        path.basename += '.min'
+    }))
+    .pipe(gulp.dest('static/css/'))
 });
---
+++ src/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  let scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+
+    if (scrollTop >= 100) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('#global-nav').removeClass('scrolled-nav');
+    }
+  });
+});
