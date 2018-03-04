e70fc6b8ea7ad36a84f9e0eaa8c4c448b0c95cb4
lllgggzzz <1196494388@qq.com>
2018-03-04 14:18:55 +0800

    fix form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### 重定向不再采用redirect, 转为返回jsonify(status=302, location=${url})
+###group的表单，以及签到的表单需要完善
+###profile/center简介未写 以及info里的生日
--- service.py
+++ service.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('test/index.html')
 
 @app.route("/post_test", methods=['POST'])
 def post_test():
