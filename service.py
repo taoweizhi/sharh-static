a86bfb4c8bd70c1bc9b91deea379c49e3f86227d
AnxQ <xiaoqi990112@hotmail.com>
2018-03-05 21:33:48 +0800

    Update layout

--- service.py
+++ service.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return render_template('test/index.html')
+    return render_template('test.html',
+                           links=[{'href': 'link', 'title': 'A'}],
+                           side_info={'avatar': '',
+                                      'name': '',
+                                      'number': ''},
+                           side_option=[{'href': '',
+                                         'icon': '',
+                                         'title': ''}])
+
 
 @app.route("/post_test", methods=['POST'])
 def post_test():
     return jsonify(status=302, location=url_for('get_test'))
 
+
 @app.route("/get_test", methods=['GET'])
 def get_test():
     return "Hello World"
+
 
 if __name__ == "__main__":
     app.debug = True
