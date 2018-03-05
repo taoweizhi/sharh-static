0bfb49599532be76e18995bef404845870388c3f
Christophe1997 <sdl_1997@163.com>
2018-03-05 19:37:07 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,27 @@ const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const gutil = require('gulp-util');
 
+const defaultLog = (err) => {
+  gutil.log(gutil.colors.red('[Error]'), err.toString());
+};
+
+const addMinToFileName = (path) => {
+  if (!path.basename.endsWith('.min'))
+    path.basename += '.min'
+};
+
 gulp.task('CompressJS', () => {
   gulp.src('dist/src/js/*.js')
     .pipe(uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .pipe(rename( path => {
-      if (!path.basename.endsWith('.min'))
-        path.basename += '.min'
-    }))
+    .on('error', defaultLog)
+    .pipe(rename(addMinToFileName))
     .pipe(gulp.dest('static/js/'))
 });
 
 gulp.task('CompressCSS', () => {
   gulp.src('src/css/*.css')
     .pipe(cleanCSS())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .pipe(rename( path => {
-      if (!path.basename.endsWith('.min'))
-        path.basename += '.min'
-    }))
+    .on('error', defaultLog)
+    .pipe(rename(addMinToFileName))
     .pipe(gulp.dest('static/css/'))
 });
