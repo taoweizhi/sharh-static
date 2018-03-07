8ddab4d6786ea52f0c13e34f8b89f3319ab20891
Christophe1997 <sdl_1997@163.com>
2018-03-07 19:45:00 +0800

    后端结合

---
+++ init.py
@@ -0,0 +1,81 @@
+from rx_sharh.states.config import Config
+from rx_sharh.dependency.aliyun_sms import AliyunSms
+import rx_sharh.states.database as db
+from werkzeug.contrib.fixers import ProxyFix
+from flask_login import current_user, login_required
+from flask_login.login_manager import LoginManager
+from flask import Flask, g, request, session, make_response, render_template, redirect, url_for, Response
+import logging
+
+
+def init(**kwargs):
+    for k, v in kwargs.items():
+        setattr(Config, k, v)
+
+    app = Flask(__name__)
+    db.init_db()
+    sms = AliyunSms(app)
+    Config.sms = sms
+    Config.app = app
+
+    app.add_template_global(str, 'str')
+    app.secret_key = Config.secret_key
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'authentication.login'
+
+    if app.debug:
+        stream_handle = logging.StreamHandler()
+        stream_handle.setFormatter(logging.Formatter(Config.logging_fmt))
+        stream_handle.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handle)
+
+    file_handle = logging.FileHandler(Config.logging_path)
+    file_handle.setFormatter(logging.Formatter(Config.logging_fmt))
+    file_handle.setLevel(logging.WARNING)
+
+    app.logger.addHandler(file_handle)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.User.query.filter_by(id=int(user_id)).first()
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template("404.html"), 404
+
+    @app.before_request
+    def load_current_user():
+        g.user = current_user
+
+    @app.teardown_request
+    def remove_db_session(exception):
+        db.db_session.remove()
+
+    @app.route('/image/<imagep>')
+    def get_image(imagep):
+        try:
+            path = str(Config.path / "sharh-static" / "static" / "images" / imagep)
+            with open(path, 'rb') as image:
+                return Response(image.read(), content_type="image/{}".format("png" if ".png" in imagep else
+                                                                             "jpeg" if ".jpg" in imagep else
+                                                                             "gif" if ".gif" in imagep else
+                                                                             ""))
+        except Exception as e:
+            logging.log(logging.WARNING, e)
+            return Response(render_template('404.html'))
+
+    from rx_sharh.service.view import attendance
+    from rx_sharh.service.view import group
+    from rx_sharh.service.view import authentication
+    from rx_sharh.service.view import schedule
+    from rx_sharh.service.view import profile
+    from rx_sharh.service.view import index
+
+    app.register_blueprint(attendance.mod)
+    app.register_blueprint(group.mod)
+    app.register_blueprint(authentication.mod)
+    app.register_blueprint(schedule.mod)
+    app.register_blueprint(profile.mod)
--- service.py
+++ service.py
@@ -1,30 +1,43 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+import sys
+sys.path.append('..')
+from init import init
+from rx_sharh.states.config import Config
+from dateutil.parser import parse
 
-app = Flask(__name__)
+conf_spec = dict(
+    debug=True,
+    testing=False,
+    csrf_enabled=True,
+    admins=['sdl.office.1997@gmail.com',  # 沈东来
+            '1196494388@qq.com',  # 李高正
+            '591486669@qq.com',  # 骆云鹏
+            'xiaoqi990112@hotmail.com'],  # 安啸琪
+    term_start_date=parse('2017/8/28'),
+    database_connect_options={},
+    secret_key=r'7\xe6\xad%\x9e3\xe6\x93#\x1cD}\xf3\xef\xb6\x83s\x86b\x81rX\xf2\x1f\xd4',
+    logging_path='sharh.log',
+    database_url='mysql+pymysql://root:12345@localhost/dev2?charset=utf8',
+    access_key_id="LTAI2t84jhtJ69vY",
+    access_key_secret="aC8rOMSLMxIU8lDwYhjblF7tCYlGZy",
+    sign_name="智慧校园",
+    template_code="SMS_117521047",
+    logging_fmt=("message type:       %(levelname)s\n"
+                 "Location:           %(pathname)s:%(lineno)d\n"
+                 "Module:             %(module)s\n"
+                 "Function:           %(funcName)s\n"
+                 "Time:               %(asctime)s\n"
+                 "message:            %(message)s\n"),
 
+    course_spec=list(zip(
+        ["8:00", "8:50", "9:40", "10:40", "11:30",
+         "14:00", "14:50", "15:50", "16:40", "17:30",
+         "19:00", "19:50", "20:40"],
+        ["8:50", "9:40", "10:25", "11:30", "12:15",
+         "14:50", "15:35", "16:40", "17:30", "18:15",
+         "19:50", "20:40", "21:25"]
+    )))
 
-@app.route("/")
-def index():
-    return render_template('test.html',
-                           links=[{'href': 'link', 'title': 'A'}],
-                           side_info={'avatar': '',
-                                      'name': '',
-                                      'number': ''},
-                           side_option=[{'href': '',
-                                         'icon': '',
-                                         'title': ''}])
-
-
-@app.route("/post_test", methods=['POST'])
-def post_test():
-    return jsonify(status=302, location=url_for('get_test'))
-
-
-@app.route("/get_test", methods=['GET'])
-def get_test():
-    return "Hello World"
-
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+init(**conf_spec)
+app = Config.app
+app.debug = True
+app.run()
