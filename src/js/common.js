353993d7e7636ce2ac7ee6d345d60d7f882f0ee8
AnxQ <xiaoqi990112@hotmail.com>
2018-03-07 21:54:35 +0800

    Fix

--- src/js/common.js
+++ src/js/common.js
@@ -1,23 +1,70 @@
-$(document).ready(function(){
-  let scrollTop = 0;
-  $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
-     $('.counter').html(scrollTop);
+$(document).ready(function () {
+    let scrollTop = 0;
+    let scrollHeight = 0;
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+        scrollHeight = $(document).height();
+        $('.counter').html(scrollTop);
 
-    if (scrollTop >= 100) {
-      $('#global-nav').addClass('scrolled-nav');
-    } else if (scrollTop < 100) {
-      $('#global-nav').removeClass('scrolled-nav');
-    }
-  });
+        if (scrollTop + windowHeight > scrollHeight - 80) {
+            $(footer).addClass('scrolled-footer');
+        } else if (scrollTop + windowHeight <= scrollHeight - 80) {
+            $(footer).removeClass('scrolled-footer');
+        }
+
+        if (scrollTop >= 100) {
+            $('#global-nav').addClass('scrolled-nav');
+            $('.page-content').addClass('scrolled-content');
+        } else if (scrollTop < 100) {
+            $('#global-nav').removeClass('scrolled-nav');
+            $('.page-content').addClass('scrolled-content');
+        }
+    });
+
 });
 
-const signinFormHandler = (result) => {};
-const loginFormHandler  = (result) => {};
-const phoneFormHandler = (result) => {};
-const profileFormHandler = (result) => {};
-const attendanceFormHandler = (result) => {};
-const reAttendanceFormHandler = (result) => {};
-const infoFormHandler = (result) => {};
-const codeSubmitFormHandler = (result) => {};
-const groupFormHandler = (result) => {};
+const signinFormHandler = (result) =
+>
+{
+}
+;
+const loginFormHandler = (result) =
+>
+{
+}
+;
+const phoneFormHandler = (result) =
+>
+{
+}
+;
+const profileFormHandler = (result) =
+>
+{
+}
+;
+const attendanceFormHandler = (result) =
+>
+{
+}
+;
+const reAttendanceFormHandler = (result) =
+>
+{
+}
+;
+const infoFormHandler = (result) =
+>
+{
+}
+;
+const codeSubmitFormHandler = (result) =
+>
+{
+}
+;
+const groupFormHandler = (result) =
+>
+{
+}
+;
